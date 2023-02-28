c79fbded6da80684876094555111c0fe67bd4b7e
Josue <josue_2707@hotmail.com>
2023-02-28 09:39:08 -0500

    fixed ruta src imagenes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { HeroesApp } from './HeroesApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <HeroesApp />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
